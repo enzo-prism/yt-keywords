7e950e22697f1a4c5ed205870e09ae1a2da6ddbb
enzo-prism <215033440+enzo-prism@users.noreply.github.com>
2025-12-26 12:40:13 -0800

    Add Hotjar tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,6 +40,16 @@ function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', 'G-KSYZ1K3PKX');`}
         </Script>
+        <Script id="hotjar" strategy="afterInteractive">
+          {`(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:6608906,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        </Script>
       </head>
       <body
         className={`${spaceGrotesk.variable} ${jetbrainsMono.variable} font-sans antialiased`}
